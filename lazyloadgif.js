df3178562ffb3c3ef3ee8a6b1d3154964b86a9d1
SHADOWisPRO07 <119752476+SHADOWisPRO07@users.noreply.github.com>
2024-03-28 15:56:54 +0530

    Create lazyloadgif.js

---
+++ lazyloadgif.js
@@ -0,0 +1,22 @@
+function loadLazyLoadLibrary(callback) {
+  const script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/vanilla-lazyload@17.6.1/dist/lazyload.min.js';
+  script.onload = callback;
+  document.head.appendChild(script);
+}
+
+function initLazyLoad() {
+  const lazyLoadInstance = new LazyLoad({
+    elements_selector: '.lazy',
+    use_native: true
+  });
+}
+
+if (window.addEventListener) {
+  window.addEventListener('load', function() {
+    loadLazyLoadLibrary(initLazyLoad);
+  });
+} else {
+  window.attachEvent('onload', function() {
+    loadLazyLoadLibrary(initLazyLoad);
+  });
